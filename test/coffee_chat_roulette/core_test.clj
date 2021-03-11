24d4e8c0385e5d8dd4cd0bad3a089cb93ec98d26
AGenson <anthony.genson@outlook.com>
2021-03-11 20:32:05 +0100

    Removed default test

--- test/coffee_chat_roulette/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns coffee-chat-roulette.core-test
-  (:require [clojure.test :refer :all]
-            [coffee-chat-roulette.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
